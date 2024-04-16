4a761f92420a015ee0dffa2175bc68a4a1af61f7
Chris Kitras <chkitras@gmail.com>
2024-04-16 17:47:11 -0600

    ready for integration

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, Response
+import os
+import random
 
 app = Flask(__name__)
 
+@app.route("/random", methods=["GET"])
+def random():
+    # Return a random song from audio
+    songs = [file for file in os.listdir("static/audio") if file.endswith(".mp3")]
+    song = random.choice(songs)
+    return Response(song, mimetype="text/plain")
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template(
+        "index.html",
+        songs=[
+            file for file in os.listdir("static/audio") if file.endswith(".mp3")
+        ],
+    )
